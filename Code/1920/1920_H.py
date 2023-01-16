9a450af580cec6c4fa66033650e98d5ec1643900
wocjs <wocjs602@gmail.com>
2023-01-16 17:46:15 +0900

    1920_H commit

---
+++ Code/1920/1920_H.py
@@ -0,0 +1,25 @@
+N = int(input())
+list_N = list(map(int, input().split()))
+list_N.sort()
+M = int(input())
+list_M = list(map(int, input().split()))
+
+def binary_search(search):
+    head = 0
+    tail = N - 1
+    while head <= tail:
+        middle = (head + tail) // 2
+        if list_N[middle] == search:
+            return True
+
+        if search < list_N[middle]:
+            tail = middle - 1
+        elif search > list_N[middle]:
+            head = middle + 1
+
+
+for i in range(M):
+    if binary_search(list_M[i]):
+        print(1)
+    else:
+        print(0)
