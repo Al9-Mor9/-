f263f6879d9550756eef1cfd055fee45e901ca6c
Haaarimmm <haaarimmm@gmail.com>
2023-01-16 14:46:47 +0900

    add 1065_K.py

---
+++ Code/1065/1065_K.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = 0
+
+for i in range(1, n+1):
+    num = list(map(int, str(i)))
+    if i < 100:
+        cnt += 1
+    elif num[1] - num[0] == num[2] - num[1]:
+        cnt += 1
+
+print(cnt)
