b7a0038c88f64959f552ac53890f11fcae513a37
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2023-01-26 10:48:02 +0900

    Create check_files.py

---
+++ utils/check_files.py
@@ -0,0 +1,34 @@
+import os
+
+def get_code_list():
+    code_list = [file for file in os.listdir("./Code")]
+    return code_list
+  
+def make_info(code_list):
+    info = {}
+    for code in code_list:
+        tup = os.path.splitext(code)
+        problem_number, author = tup[0].split('_')
+        info[problem_number].append(author)
+        
+    return info
+
+def update_readme(readme):
+    code_list = get_code_list()
+    info = make_info(code_list)
+    while True:
+        line = readme.readline()
+        if not line : break
+        for problem_number in info.keys:
+            if problem_number in line:
+                for author in info[problem_number]:
+                    line = line.rstrip() + author
+                    print(line)
+                break
+    return readme
+
+
+if __name__ == "__main__":
+    readme = open("./README.md", 'a', encoding='utf-8')
+    update_readme(readme)
+    readme.close()
