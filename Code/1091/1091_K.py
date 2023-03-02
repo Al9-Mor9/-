57246ee5925299174f91ff55d9173aab69faaf6a
Haaarimmm <haaarimmm@gmail.com>
2023-03-02 13:30:36 +0900

    1091_K.py

---
+++ Code/1091/1091_K.py
@@ -0,0 +1,21 @@
+# 카드 섞기
+import sys
+sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+p = list(map(int, input().split()))
+s = list(map(int, input().split()))
+target = [0, 1, 2] * (n//3)
+card = [0] * n
+origin = p[:]
+
+cnt = 0
+while p != target:
+    for i in range(n):
+        card[s[i]] = p[i]
+    p = card[:]
+    cnt += 1
+    if p == origin:
+        cnt = -1
+        break
+print(cnt)
